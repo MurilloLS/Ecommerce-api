62f20a233f2564ec73ae09f56bfaa31c04ced775
Murillo Santos <murillo.lds01@gmail.com>
2024-08-30 13:09:19 -0300

    fix: Update Product and Category models to align with DTO changes and ensure proper data handling

--- Models/Product.cs
+++ Models/Product.cs
@@ -7,11 +7,9 @@ namespace ECommerceApi.Models
     public class Product
     {
         public Guid Id { get; set; }
-        [Required]
-        public string Name { get; set; }
-        [Required]
+        public string? Name { get; set; }
         public decimal Price { get; set; }
         public Guid CategoryId { get; set; }
-        public Category Category { get; set; }
+        public Category? Category { get; set; }
     }
 }
