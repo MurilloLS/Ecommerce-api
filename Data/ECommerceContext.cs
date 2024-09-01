1d6304258746d0066895c08ad99ac7d0539c5827
Murillo Santos <murillo.lds01@gmail.com>
2024-08-31 21:43:41 -0300

    feat: Add DbSet of the new entities

--- Data/ECommerceContext.cs
+++ Data/ECommerceContext.cs
@@ -10,10 +10,19 @@ namespace ECommerceApi.Data
         {
         }
 
+        public DbSet<User> Users { get; set; }
+        public DbSet<ShoppingCart> ShoppingCarts { get; set; }
+        public DbSet<ShoppingCartItem> ShoppingCartItems { get; set; }
         public DbSet<Product> Products { get; set; }
         public DbSet<Category> Categories { get; set; }
-        public DbSet<User> Users { get; set; }
-        public DbSet<ShoppingCartItem> ShoppingCartItems { get; set; }
-        public DbSet<Order> Orders { get; set; }
+
+        protected override void OnModelCreating(ModelBuilder modelBuilder)
+        {
+            base.OnModelCreating(modelBuilder);
+
+            modelBuilder.Entity<Product>()
+                .Property(p => p.Price)
+                .HasColumnType("decimal(18,2)"); 
+        }
     }
 }
